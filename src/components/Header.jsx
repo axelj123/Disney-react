2a666d19eb5e0ea4bee544a5b659d980188e14d5
axelj123 <axeljhosmell13@gmail.com>
2024-09-27 23:42:18 -0500

    m

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,3 @@
-
 import React, { useState } from 'react';
 import logo from './../assets/img/disneylogo.png';
 import {
@@ -8,7 +7,7 @@ import { HiPlus, HiDotsVertical } from "react-icons/hi";
 import HeaderItem from './HeaderItem';
 
 function Header() {
-const [toggle,setToggle]=useState(false);
+    const [toggle, setToggle] = useState(false);
 
     const menu = [
         {
@@ -35,35 +34,44 @@ const [toggle,setToggle]=useState(false);
             name: 'SERIES',
             icon: HiTv
         }
-    ]
+    ];
+
     return (
-        <div className='flex items-center gap-8 justify-between p-5'>
-            <div className='flex gap-8 items-center'>
-                <img src={logo} className='w-[80px] md:w-[115px] object-cover' />
+        <div className='flex items-center justify-between p-5 bg-black text-white'>
+            <div className='flex items-center gap-8'>
+                <img src={logo} className='w-[80px] md:w-[115px] object-cover' alt="Logo" />
+                
                 <div className='hidden md:flex gap-8'>
                     {menu.map((item) => (
-                        <HeaderItem name={item.name} Icon={item.icon} />
+                        <HeaderItem key={item.name} name={item.name} Icon={item.icon} />
                     ))}
                 </div>
-                <div className='flex md:hidden gap-8'>
-                    {menu.map((item, index) => index < 3 && (
-                        <HeaderItem name={''} Icon={item.icon} />
+
+                <div className='flex md:hidden relative'>
+                    {menu.slice(0, 3).map((item) => (
+                        <HeaderItem key={item.name} name={''} Icon={item.icon} />
                     ))}
-                    <div className='md:hidden' onClick={()=>setToggle(!toggle)}>
+                    <div onClick={() => setToggle(!toggle)}>
                         <HeaderItem name={''} Icon={HiDotsVertical} />
-                       {toggle?<div className='absolute mt-3 bg-[#121212] 
-                        border-[1px] border-gray-700 p-3 px-2 py-4 '>
-                            {menu.map((item, index) => index > 2 && (
-                                <HeaderItem name={item.name} Icon={item.icon} />
-                            ))}
-                        </div>:null} 
                     </div>
 
+                    {toggle && (
+                        <div className='absolute mt-3 bg-[#121212] border border-gray-700 p-3 px-2 py-4 rounded-md z-10'>
+                            {menu.slice(3).map((item) => (
+                                <HeaderItem key={item.name} name={item.name} Icon={item.icon} />
+                            ))}
+                        </div>
+                    )}
                 </div>
             </div>
-            <img src="https://ps.w.org/user-avatar-reloaded/assets/icon-256x256.png?rev=2540745"
-                className='w-[40px] rounded-full' />
+
+            <img
+                src="https://ps.w.org/user-avatar-reloaded/assets/icon-256x256.png?rev=2540745"
+                className='w-[40px] rounded-full'
+                alt="User Avatar"
+            />
         </div>
-    )
+    );
 }
-export default Header
+
+export default Header;
