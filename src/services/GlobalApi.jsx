c92caa2a00fdb6e9d130fe449cfd9132643e2a02
axelj123 <axeljhosmell13@gmail.com>
2024-09-29 01:05:22 -0500

    f

--- src/services/GlobalApi.jsx
+++ src/services/GlobalApi.jsx
@@ -1,11 +1,10 @@
 import axios from "axios";
 
-const movieBaseUrl = "https://api.themoviedb.org/3";
-const api_key = 'b2c46d4040f57817267909e152883df9';
+const API = import.meta.env.VITE_API; // Cambia esto
+const API_KEY = import.meta.env.VITE_API_KEY; // Cambia esto
 
-// Define getTrendingVideos como una función que retorna la promesa de axios
 const getTrendingVideos = () => {
-    return axios.get(`${movieBaseUrl}/trending/all/day?api_key=${api_key}`);
+    return axios.get(`${API}/trending/all/day?api_key=${API_KEY}`);
 };
 
 export default {
