41deff7fd55ebe9475c2d97bb9b86ba0110598af
axelj123 <axeljhosmell13@gmail.com>
2024-10-08 16:25:41 -0500

    empezando con los cards

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { useState } from 'react'
 import './App.css'
 import Header from './components/Header'
 import Slider from './components/Slider'
+import ProductionHouse from './components/ProductionHouse'
 function App() {
   const [count, setCount] = useState(0)
 
@@ -10,6 +11,7 @@ function App() {
     <div className="">
       <Header/>
       <Slider/>
+      <ProductionHouse/>
     </div>
   )
 }
---
+++ src/components/ProductionHouse.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import disney from './../assets/img/disney.png'
+import marvel from './../assets/img/marvel.png'
+import nationalG from './../assets/img/nationalG.png'
+import pixar from './../assets/img/pixar.png'
+import starwar from './../assets/img/starwar.png'
+
+import disneyV from './../assets/videos/disney.mp4'
+import marvelV from './../assets/videos/marvel.mp4'
+import nationalGV from './../assets/videos/national-geographic.mp4'
+import pixarV from './../assets/videos/pixar.mp4'
+import starwarV from './../assets/videos/star-wars.mp4'
+
+
+
+
+
+function ProductionHouse() {
+    return (
+        <div>ProductionHouse</div>
+    )
+}
+
+export default ProductionHouse
